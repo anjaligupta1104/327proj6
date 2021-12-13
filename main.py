5c00a4b4d4a60eb5dccb80b100bd723286f94c10
anjaligupta1104 <anjaligupta1104@gmail.com>
2021-12-13 00:05:46 -0500

    created main.py

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ Meili and Anjali Gupta
 CPSC 327 Pset
 Saturday, 3:30 pm
 """
+
+import sys
+from SantoriniCLI import SantoriniCLI
+
+"""
+Take in the command line args, then use them to set up a manager object that will drive the core game loop.
+"""
+
+if __name__ == "__main__":
+    SantoriniCLI(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]).run()
