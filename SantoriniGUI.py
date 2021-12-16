c5cd358a3fd7a86a18832aa8a4ca1b6be87ab101
anjaligupta1104 <anjaligupta1104@gmail.com>
2021-12-16 17:21:21 -0500

    added functionality for buttons and labels below grid

--- SantoriniGUI.py
+++ SantoriniGUI.py
@@ -1,4 +1,3 @@
-import sys
 from board import Board
 from player import playerFactory
 from memento import Memento
@@ -12,16 +11,13 @@ class SantoriniGUI():
     def __init__(self, arg1, arg2, arg3, arg4):
         self._window = tk.Tk()
         self._window.title("Santorini")
-        self._window.geometry("5000x5000")
-        
-        self._board_frame = tk.Frame(self._window)
-        self._board_frame.grid(row=0, column=1, columnspan=5) # frame for 5x5 board grid
-        self._turn_frame = tk.Frame(self._window)
-        self._turn_frame.grid(row=1, column=1, columnspan=2) # frame for turn and player
-        self._undo_frame = tk.Frame(self._window)
-        self._undo_frame.grid(row=2, column=1, columnspan=3) # frame for optional undo/redo/next buttons
-        self._score_frame = tk.Frame(self._window)
-        self._score_frame.grid(row=3, column=1, columnspan=3) # frame for optional score display
+        self._window.geometry("5000x5000")
+
+        self._board_frame = tk.Frame(self._window)
+        self._board_frame.grid(row=0, column=1, columnspan=5) # frame for 5x5 board grid
+
+        self._turn_frame = tk.Frame(self._window)
+        self._turn_frame.grid(row=1, column=1, columnspan=2) # frame for turn and player
 
         self.board = Board()
         player1 = playerFactory.build_player(self.board, arg1, 1)
@@ -37,28 +33,43 @@ class SantoriniGUI():
 
         # TODO: implement grid buttons for clicking and add workers
 
+        if self._turn % 2 == 1:
+            playerLabel = "white (AB)"
+        else:
+            playerLabel = "blue (YZ)"
+
+        turn_text = f"Turn: {self._turn}, {playerLabel}"
+        tk.Label(self._turn_frame, 
+                text=turn_text).grid(row=1, column=1, columnspan=2)
+
+        if self._redo:
+            self._undo_frame = tk.Frame(self._window)
+            self._undo_frame.grid(row=2, column=1, columnspan=3) # frame for optional undo/redo/next buttons
+
+            tk.Button(self._undo_frame, 
+                    text="Next", 
+                    command=self._next_move).grid(row=1, column=1)
+            tk.Button(self._undo_frame, 
+                    text="Undo", 
+                    command=self._undo_move).grid(row=1, column=2)
+            tk.Button(self._undo_frame, 
+                    text="Redo", 
+                    command=self._redo_move).grid(row=1, column=2)
+
+        if self._score:
+            self._score_frame = tk.Frame(self._window)
+            self._score_frame.grid(row=3, column=1) # frame for optional score display
+
+            (move_score, height_score, center_score, distance_score) = self._currPlayer._move_score()
+            score_text = f"{height_score}, {center_score}, {distance_score}"
+
+            tk.Label(self._score_frame, 
+                    text=score_text).grid(row=1, column=1)
+
         self._window.mainloop()
     
     # def run(self, caretaker):
     #     while True:
-    #         # display
-    #         self.board.display()
-
-    #         # print turn and player
-    #         if self._turn % 2 == 1:
-    #             playerLabel = "white (AB)"
-    #         else:
-    #             playerLabel = "blue (YZ)"
-
-    #         print("Turn: {0}, {1}".format(self._turn, playerLabel), end = '')
-
-    #         # print score, note that this only occurs if currPlayer is heuristic
-    #         if self._score:
-    #             (move_score, height_score, center_score, distance_score) = self._currPlayer._move_score()
-    #             print(", ({0}, {1}, {2})".format(height_score, center_score, distance_score))
-    #         else:
-    #             print("")
-
     #         # check if game ended
     #         if self.board.game_ended():
     #             sys.exit(0)
@@ -94,6 +105,8 @@ class SantoriniGUI():
     #             self._currPlayer = self.board.player1
     #             self._otherPlayer = self.board.player2
 
+
+# TOOD: implement undo/redo functionality with Memento
     # def save(self):
     #     """
     #     Saves the current state inside a memento.
--- mainGUI.py
+++ mainGUI.py
@@ -8,4 +8,4 @@ from SantoriniGUI import SantoriniGUI
 from memento import Memento, Caretaker
 
 if __name__ == "__main__":
-    SantoriniGUI()
+    SantoriniGUI(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
